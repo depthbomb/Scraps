09165f51033ecaa9b3a153ceee0b92f691fa1a4b
depthbomb <reesepup77@hotmail.com>
2020-08-27 15:59:53 -0500

    Version hotfix

--- Scraps.Common/AppVersion.cs
+++ Scraps.Common/AppVersion.cs
@@ -34,7 +34,7 @@ namespace Scraps.Common
         public static int Minor => 3;
         public static int Patch => 1;
         public static int Hotfix => 0;
-        public static ReleaseTypes ReleaseType => ReleaseTypes.PreRelease;
+        public static ReleaseTypes ReleaseType => ReleaseTypes.Release;
         public static string SemVer => $"{Major}.{Minor}.{Patch}.{Hotfix}";
         public static string Full => $"{SemVer}-{ReleaseType}";
     }
