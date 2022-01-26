b88346ebf30986cedab5b1e75867ecb8fd4706c2
depthbomb <reesepup77@hotmail.com>
2022-01-26 16:42:07 -0600

    refactor(Utils): add owner to message boxes

--- Scraps.GUI/Utils.cs
+++ Scraps.GUI/Utils.cs
@@ -16,22 +16,20 @@
 /// along with this program. If not, see <https://www.gnu.org/licenses/>.
 #endregion License
 
-using System.Windows.Forms;
-
 namespace Scraps.GUI
 {
     public class Utils
     {
-        public static DialogResult ShowQuestion(string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
-            => MessageBox.Show(content, title, buttons, MessageBoxIcon.Question);
+        public static DialogResult ShowQuestion(IWin32Window owner, string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
+            => MessageBox.Show(owner, content, title, buttons, MessageBoxIcon.Question);
 
-        public static DialogResult ShowInfo(string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
-            => MessageBox.Show(content, title, buttons, MessageBoxIcon.Information);
+        public static DialogResult ShowInfo(IWin32Window owner, string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
+            => MessageBox.Show(owner, content, title, buttons, MessageBoxIcon.Information);
 
-        public static DialogResult ShowWarning(string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
-            => MessageBox.Show(content, title, buttons, MessageBoxIcon.Warning);
+        public static DialogResult ShowWarning(IWin32Window owner, string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
+            => MessageBox.Show(owner, content, title, buttons, MessageBoxIcon.Warning);
 
-        public static DialogResult ShowError(string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
-            => MessageBox.Show(content, title, buttons, MessageBoxIcon.Error);
+        public static DialogResult ShowError(IWin32Window owner, string title, string content, MessageBoxButtons buttons = MessageBoxButtons.OK)
+            => MessageBox.Show(owner, content, title, buttons, MessageBoxIcon.Error);
     }
 }
