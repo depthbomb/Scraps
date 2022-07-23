dd93b89dac1c7ee042254f9650001d5f24362af6
depthbomb <reesepup77@hotmail.com>
2022-07-22 21:47:57 -0500

    refactor: optimize regex

--- Scraps.GUI/Constants/RegexPatterns.cs
+++ Scraps.GUI/Constants/RegexPatterns.cs
@@ -22,12 +22,12 @@ namespace Scraps.GUI.Constants
 {
     public class RegexPatterns
     {
-        public static readonly Regex CSRF = new(@"ScrapTF\.User\.Hash = ""([a-f0-9]{64})"";", RegexOptions.Compiled);
+        public static readonly Regex CSRF = new(@"ScrapTF\.User\.Hash = ""([a-f0-9]{64})"";");
         public static readonly Regex RAFFLE_ENTRY = new(@"ScrapTF\.Raffles\.RedirectToRaffle\('([A-Z0-9]{6,})'\)", RegexOptions.Compiled);
         public static readonly Regex RAFFLE_HASH = new(@"EnterRaffle\('[A-Z0-9]{6,}', '([a-f0-9]{64})'", RegexOptions.Compiled);
         public static readonly Regex RAFFLE_LIMIT = new(@"total=""(\d+)"" data-max=""(\d+)", RegexOptions.Compiled);
         public static readonly Regex WON_RAFFLES_ALERT = new(@"You've won \d raffles? that must be withdrawn", RegexOptions.Compiled);
-        public static readonly Regex BAN_REASON = new(@"<b>Reason:<\/b> ([\w\s]+)", RegexOptions.Compiled);
+        public static readonly Regex BAN_REASON = new(@"<b>Reason:<\/b> ([\w\s]+)");
         public static readonly Regex HONEYPOT_RAFFLE_STYLE = new(@"<style>\.enter-raffle-btns \.btn{(.*)}<\/style>", RegexOptions.Compiled);
         public static readonly Regex HONEYPOT_RAFFLE_MAX_ENTRIES = new(@"data-max=""(\d{1,})""", RegexOptions.Compiled);
         public static readonly Regex HONEYPOT_RAFFLE_BANNED_USERS = new(@"<img class='tiny-raffle-avatar\s?' style='border-color:\s?#CC1100;?' src='(.*)' loading=""lazy""\s?\/>", RegexOptions.Compiled);
